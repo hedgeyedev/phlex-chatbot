000b3bbaca2d633d1b69c459a533ae1a127ce73f
Christopher Chang <cchang@hedgeye.com>
2024-11-18 20:06:31 -0800

    [#188571675] feat: Restore textarea auto-resizing

--- src/javascript/controllers/chat_form_controller.js
+++ src/javascript/controllers/chat_form_controller.js
@@ -2,23 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="pcb-chat-form"
 export default class extends Controller {
+  static targets = ["input"];
 
   connect() {
-    // console.log("pcb ChatFormController connected")
     // clear form
-
     this.element.reset()
   }
 
-  // Add behavior to submitting the form:
-  // 1. Change the text
-  // 2. Prevent the default form submission
-  // 3. disable the form
-  // 4. Send the form
-  // submitForm(event) {
-  //   this.submitButtonTarget.textContent = "Sending..."
-  //   event.preventDefault()
-  //   this.formTarget.disabled = true
-  //   this.formTarget.requestSubmit()
-  // }
+  resize() {
+    this.inputTarget.style.height = 'auto'
+    this.inputTarget.style.height = this.inputTarget.scrollHeight + 'px'
+  }
 }
